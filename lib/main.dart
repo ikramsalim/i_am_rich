ad9ea6f5db98e94d5d18c501a45df2208fb7d451
Ayrton Ikram <ikram@ayrtons-mbp.gateway.sasktel.net>
2020-02-28 01:08:45 -0600

    -created an app bar , made it dark blue -gave it a title using a text widget -set the background  of our app to a colour that comes from the material.io/design -added a body property to our scaffold to tell it what to display - fetched an image from the internet  using Network Image(url) then centred it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+//CREATED BY IKRAM
+// when creating a widget inside a widget, add a comma after every end of round  bracket
+// so as to have a neat code style
+// click reformat code with dartfmt or hit enter
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'),
         ),
       ),
-    );
+    ),
+  ));
+}
